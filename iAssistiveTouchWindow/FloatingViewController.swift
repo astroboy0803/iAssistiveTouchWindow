3228be67ac831ea1ecc86560658f8f19b9358e16
黃宇新 <astroboy03@cathaylife.com.tw>
2021-10-05 13:55:44 +0800

    test flow

--- iAssistiveTouchWindow/FloatingViewController.swift
+++ iAssistiveTouchWindow/FloatingViewController.swift
@@ -59,10 +59,14 @@ internal final class FloatingViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         snapButtonToSocket()
